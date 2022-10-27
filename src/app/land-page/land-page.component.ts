abbfa9a9893b7548779b7584ba12c92b1afec41e
Henrique Lisboa Brugnarotto <henriquebrug@outlook.com>
2022-10-27 00:14:07 -0300

    Button adicionar saldo

--- src/app/land-page/land-page.component.ts
+++ src/app/land-page/land-page.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-land-page',
   templateUrl: './land-page.component.html',
-  styleUrls: ['./land-page.component.css']
+  styleUrls: ['./land-page.component.css'],
 })
 export class LandPageComponent implements OnInit {
+  valor: number = 0;
+  mostrarSoma = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  expandirSoma() {
+    this.mostrarSoma = true;
   }
 
+  realizarSoma() {
+    this.mostrarSoma = false;
+  }
 }
